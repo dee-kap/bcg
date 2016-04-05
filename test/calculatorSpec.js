7beb08974b90ab0d3fd2f1d7eb0ee05f00c2b255
Deepak Kapoor <kapoordeepak@gmail.com>
2016-04-05 17:14:28 +1000

    Lib implemented

--- test/calculatorSpec.js
+++ test/calculatorSpec.js
@@ -1,8 +1,50 @@
 'use strict';
 var assert = require('chai').assert;
-var calculator = require('../lib/calculator.js');
+var Calculator = require('../lib/calculator.js');
 
 describe('Calculator', function() {
+
+  describe('calculateDays', function() {
+
+    it('should return 19', function() {
+      var days = Calculator.calculateDays('02/06/1983', '22/06/1983');
+      assert.equal(days, 19);
+    });
+
+    it('should return 173', function() {
+      var days = Calculator.calculateDays('04/07/1984', '25/12/1984');
+      assert.equal(days, 173);
+    });
+
+    it('should return 1979', function() {
+      var days = Calculator.calculateDays('03/08/1983', '03/01/1989');
+      assert.equal(days, 1979);
+    });
+
+    it('should return 1979', function() {
+      var days = Calculator.calculateDays('03/01/1989', '03/08/1983');
+      assert.equal(days, 1979);
+    });
+
+    it('should return 0', function() {
+      var days = Calculator.calculateDays('03/01/1989', '03/01/1989');
+      assert.equal(days, 0);
+    });
+
+    it('should return 0', function() {
+      var days = Calculator.calculateDays('7/11/1972', '8/11/1972');
+      assert.equal(days, 0);
+    });
+
+
+    it('should return 1', function() {
+      var days = Calculator.calculateDays('01/01/2000', '03/01/2000');
+      assert.equal(days, 1);
+    });
+
+  });
+
+
 
 
 });
--- test/sciDateSpec.js
+++ test/sciDateSpec.js
@@ -1,4 +1,5 @@
 var assert = require('chai').assert;
+var expect = require('chai').expect;
 var _ = require('lodash');
 var SciDate = require('../lib/sciDate.js');
 
@@ -9,7 +10,78 @@ describe('SciDate', function() {
   it('should create an object when valid values are passed', function() {
     var date = new SciDate(12, 3, 1987);
     assert.isTrue(_.isObject(date));
-
   });
+
+  it('should throw an error when invalid values are passed into constructor', function() {
+    expect(function(){
+      new SciDate('xx/01/1999');
+    }).to.throw('Invalid Date');
+
+  })
+
+  describe('isGreaterThan', function() {
+    it('should return true when date passed in is greater', function() {
+      var date1 = new SciDate(01, 11, 1965);
+      var date2 = new SciDate(30, 10, 1965);
+      assert.isTrue(date1.isGreaterThan(date2));
+    });
+
+    it('should return false when date passed in is NOT greater', function() {
+      var date1 = new SciDate(01, 11, 1965);
+      var date2 = new SciDate(02, 11, 1965);
+      assert.isFalse(date1.isGreaterThan(date2));
+    });
+
+  })
+
+  describe('isEqualTo', function() {
+    it('should return false when two dates are different', function() {
+      var date1 = new SciDate(01, 11, 1965);
+      var date2 = new SciDate(30, 10, 1965);
+      assert.isFalse(date1.isEqualTo(date2));
+    });
+
+    it('should return false when two dates are different', function() {
+      var date1 = new SciDate(02, 06, 1983);
+      var date2 = new SciDate(22, 06, 1983);
+      assert.isFalse(date1.isEqualTo(date2));
+    });
+
+
+    it('should return true when two dates are same', function() {
+      var date1 = new SciDate(26, 09, 2000);
+      var date2 = new SciDate(26, 09, 2000);
+      assert.isTrue(date1.isEqualTo(date2));
+    });
+
+  })
+
+  describe('addDay', function() {
+    it('should increase date by one day', function() {
+      var date = new SciDate(01, 11, 1936);
+      date.addDay();
+      assert.isTrue(date.day.day === 2);
+      assert.isTrue(date.month.month === 11);
+      assert.isTrue(date.year.year === 1936);
+    });
+
+    it('should increase date by one day', function() {
+      var date = new SciDate(31, 01, 1936);
+      date.addDay();
+      assert.isTrue(date.day.day === 1);
+      assert.isTrue(date.month.month === 2);
+      assert.isTrue(date.year.year === 1936);
+    });
+
+    it('should increase date by one day', function() {
+      var date = new SciDate(31, 12, 1936);
+      date.addDay();
+      assert.isTrue(date.day.day === 1);
+      assert.isTrue(date.month.month === 1);
+      assert.isTrue(date.year.year === 1937);
+    });
+
+
+  })
 
 });
